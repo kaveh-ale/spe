2e641fbe7a919c40d8fefe8c286d3ef123901e8e
kaveh-ale <kaveh.majidi@al-enterprise.com>
2020-06-09 13:28:01 -0700

    updates

---
+++ pe1/show_system.py
@@ -0,0 +1,38 @@
+########  Example of Python and paramiko to connect to AOS ##################
+########  Version 1.0                                                                                          ##################
+########  Author: Kaveh Majidi , SE Team
+######## Example of connecting to switch using Python Paramiko and pull system information
+
+import paramiko
+import yaml
+
+######  Loading the list of switches and their IP/User/Password from yaml file #####
+with open('switch_list.yaml') as file:
+    switch_list=yaml.load(file)
+
+##### Starting a loop to perform the following on each switch  #####
+print("##########        Operation Started.........  #############")
+print("Example of connecting to switch using CLI API and pull VLAN table")
+
+for switch in switch_list:
+    ip=switch_list[switch]['ip']
+    username=switch_list[switch]['username']
+    password=switch_list[switch]['password']
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip, username=username, password=password, look_for_keys=False, allow_agent=False)
+        print ("Connected to "  +  ip)
+        # Send the command (non-blocking)
+        command="show system"
+        stdin, stdout, stderr = ssh.exec_command(command)
+        for line in stdout.read().splitlines():
+            print(line)
+    except paramiko.AuthenticationException:
+        print ("Authentication failed when connecting to " + ip)
+        sys.exit()
+    except:
+        print ("Could not SSH to %s, waiting for it to start" + ip)
+        sys.exit()
+    print ("Command done, closing SSH connection")
+    ssh.close()
